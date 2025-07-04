776400190559ad3efe3923bb652454ea7952e7cd
Daniel A. Galindo Reyes <mseragonf@gmail.com>
2025-07-03 18:29:01 -0600

    Correccion de rutas de imagenes de certificados

--- src/app/(portafolio)/AboutMe/components/certificatesData.ts
+++ src/app/(portafolio)/AboutMe/components/certificatesData.ts
@@ -2,25 +2,25 @@
 export const CERTIFICATES = [
   {
     id: 1,
-    image: "/images/certificates/1.jpg",
+    image: "/images/Certificates/1.jpg",
     alt: "Certificado CCNA",
     title: "curso de introducción a las redes"
   },
   {
     id: 2,
-    image: "/images/certificates/2.jpg",
+    image: "/images/Certificates/2.jpg",
     alt: "Certificado CCNA",
     title: "curso de redes empresariales, seguridad y automatización"
   },
   {
     id: 3,
-    image: "/images/certificates/3.jpg",
+    image: "/images/Certificates/3.jpg",
     alt: "Certificado CCNA",
     title: "Curso de fundamentos de redes"
   },
   {
     id: 4,
-    image: "/images/certificates/4.jpg",
+    image: "/images/Certificates/4.jpg",
     alt: "Networking Essentials",
     title: "Networking Essentials"
   }
