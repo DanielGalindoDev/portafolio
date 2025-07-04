b9ce6c5797bfa945fd155f86a32ea6793e5589fd
Daniel A. Galindo Reyes <mseragonf@gmail.com>
2025-07-03 18:22:23 -0600

    Renderizamos los certificados

--- src/app/(portafolio)/AboutMe/components/certificatesData.ts
+++ src/app/(portafolio)/AboutMe/components/certificatesData.ts
@@ -2,20 +2,26 @@
 export const CERTIFICATES = [
   {
     id: 1,
-    image: "/images/certificates/CCNAIntroduccionaLasRedes.jpg",
+    image: "/images/certificates/1.jpg",
     alt: "Certificado CCNA",
-    title: "CCNAENSA"
+    title: "curso de introducción a las redes"
   },
   {
     id: 2,
-    image: "/images/certificates/CCNARedesEmprearialesSeguridadyAutomatizacion.jpg",
+    image: "/images/certificates/2.jpg",
     alt: "Certificado CCNA",
-    title: "CCNAITIN"
+    title: "curso de redes empresariales, seguridad y automatización"
   },
   {
     id: 3,
-    image: "/images/certificates/NetworkingEssentials.jpg",
+    image: "/images/certificates/3.jpg",
     alt: "Certificado CCNA",
-    title: "CCNASRWE"
+    title: "Curso de fundamentos de redes"
+  },
+  {
+    id: 4,
+    image: "/images/certificates/4.jpg",
+    alt: "Networking Essentials",
+    title: "Networking Essentials"
   }
 ] as const;
